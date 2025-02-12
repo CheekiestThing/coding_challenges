17e356a9a6fc6b41779e36f2ca76f66d42836b34
BrevonJr <romanheus1999@gmail.com>
2025-02-12 12:00:38 +0100

    Update connect_four.py

--- connect-four/connect_four.py
+++ connect-four/connect_four.py
@@ -1,12 +1,18 @@
 from colorama import Fore
+import re
 
 '''
     Game-related variables
 '''
 
+horizontal_pattern = r"(\[●\]\s){4,6}"
+diagonal_left_pattern = r"(\[[●]\]([\s\S]{4}){4}[\s\S]){4,6}"
+diagonal_right_pattern = r"(\[[●]\]([\s\S]{4}){6}[\s\S]){4,6}"
+win_pattern = horizontal_pattern + "|" + diagonal_left_pattern + "|" + diagonal_right_pattern
+
 game_running = True         # as long as it's True, the round is still on-going
-game_won = False            
-current_player = 0          # 0 = player, 1 = computer
+winner = 0                  # used after the round is over, to remember who won            
+current_player = 0
 
 # Board settings
 board_rows = 6
@@ -14,41 +20,60 @@ board_collumns = 6
 
 # String definitions for the slots and filled slots
 empty_slot = "[ ]"
-p1_token = "[" + Fore.RED + "●" + Fore.WHITE + "]"
-p2_token = "[" + Fore.BLUE + "●" + Fore.WHITE + "]"
+p1_key = "1" # r -> RED
+p2_key = "2" # b -> BLUE
 
+# Functions
 '''
-    Game-related variables
+place_token 
+    Tries to place a token to the board; If it can't, return False
+    _row = the row to try placing the token in
+    _key = the key to use (p1_key or p2_key)
 '''
-
-def place_token(_row, _token):
+def place_token(_row: int, _key : str):
     if (_row < board_collumns):
         for row in reversed(board):
             if row[_input] == empty_slot:
-                row[_input] = _token
+                row[_input] = row[_input].replace(" ", _key)
                 return True
     print(f"Token couldn't be placed at row {_row + 1}")
     return False
 
-def draw_board():
+'''
+full_board
+    Returns the full board as a simple string representation
+'''
+def full_board():
+    _output = ""
     for row in board:
-        print(" ".join(row))
-
-def check_win(_check):
-    _result = "".join(_check)
-    if p1_token + p1_token + p1_token + p1_token in _result:
-        return 1
-    elif p2_token + p2_token + p2_token + p2_token in _result:
-        return 2
-    else:
-        return 0
+        _output += (" ".join(row))
+        _output += "\n"
+    return _output
 
 '''
-    Create the board
+print_board
+    Print the board to the command line WITH the keys/tokens replaced with their actual colored symbols
 '''
+def print_board():
+    print(full_board().replace(p1_key, Fore.RED + "●" + Fore.WHITE).replace(p2_key, Fore.BLUE + "●" + Fore.WHITE), end="")
+
+'''
+print_message
+    Print a message to the command line WITH the player names being colored in
+    _message = the message to print out
+'''
+def print_message(_message):
+    print(_message.replace("Player 1", Fore.RED + "Player 1" + Fore.WHITE).replace("Player 2", Fore.BLUE + "Player 2" + Fore.WHITE))
+
+'''
+check_win
+    Utilizes RegEx (using the combined win_pattern) to see if a player has four tiles connect;
+    _key = the key to use (p1_key or p2_key)
+'''
+def check_win(_key : str):
+     return len(re.findall(win_pattern, full_board().replace(_key, "●"))) > 0
 
 board = []
-
 for row in range(0, board_rows):
     board.append([])
     for collumn in range(0, board_collumns): 
@@ -56,74 +81,51 @@ for row in range(0, board_rows):
 
 while game_running:
 
-    # Draw the game's board if the player's up
-    if not current_player == 1:
-        draw_board()
+    # Diplay the game board
+    print_board()
     
     '''
         Play the actual rounds, switch between the players during each loop
     '''
-    if current_player == 0:
-        # Ask for the player's input
-        valid_input = False
-        while not valid_input:
-            _input = int(input(f"Where do you want to place the next token? (1-{board_collumns})\n>>>")) - 1
-            
-            if (place_token(_input, p1_token)):
-                valid_input = True
-        current_player = 1
+    # Ask for the player's input
+    valid_input = False
+    _input = None
 
-    elif current_player == 1:
-        # Ask for the computer's input
-        valid_input = False
-        while not valid_input:
-            _input = int(input(f"Player 2 (1-{board_collumns})\n>>>")) - 1
-            #_input = 0 #random.randrange(1, 8, 1)
-            
-            if (place_token(_input, p2_token)):
+    # Let the player not go on until they have made a valid move
+    while not valid_input:
+        # Exit the program if anything other than an integer has been passed
+        try:
+            print_message(f"\rPlayer {current_player + 1}; Where do you want to place the next token?")
+            _input = int(input(">>>")) - 1
+        except:
+            exit()
+
+        # Try placing a token, then switch players. If it didn't work, ask for a new collum to place a token in 
+        if current_player == 0:
+            if (place_token(_input, p1_key)):
                 valid_input = True
-        current_player = 0
+                current_player = 1
+        else:
+            if (place_token(_input, p2_key)):
+                valid_input = True
+                current_player = 0
 
     '''
         Check winning conditions
     '''
-    _check = []
-    # Horziontal matches
-    for row in board:
-        for collumn in row:
-            _check.append(collumn)
-
-    _check.append(".")
-    # Vertical matches
-    for collumn in range(0, board_rows):
-        for row in range(0, board_collumns):
-            _check.append(board[row][collumn])
-
-    _check.append(".")
-    # Diagonal matches
-    hori_offset = 0
-    while hori_offset < board_collumns-3:
-        vert_offset = 0
-        while vert_offset < board_rows-3:
-            _check.append(board[hori_offset+vert_offset][vert_offset])
-            vert_offset += 1
-        hori_offset += 1
-
-    if check_win(_check) == 1:
-        game_won = True
+    if check_win(p1_key):
+        winner = 1
         game_running = False
-        print(_check)
-    elif check_win(_check) == 2:
+    elif check_win(p2_key):
+        winner = 2
         game_running = False
-        print(_check)
-
 
 '''
     Game End
 '''
-draw_board()
+print_board()
 
-if game_won:
-    print("\rCongratulations!, you won!")
+if winner != 0:
+    print_message(f"\rCongratulations, Player {winner}!, you won!")
 else:
-    print("You unfortunately failed the game.")
+    print_message("Tied!")
